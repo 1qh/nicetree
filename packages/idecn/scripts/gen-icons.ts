cd647de67e9754f51e97c3f887b076da48a68acd
Lai Quang Huy <laiquanghuy24122001@gmail.com>
2026-04-06 15:02:49 +0700

    fix: replace non-null assertions with nullish coalescing in gen-icons
    Former-commit-id: 5abf5619b83bde75b026b0f5c1fab1e3ba732996

--- packages/idecn/scripts/gen-icons.ts
+++ packages/idecn/scripts/gen-icons.ts
@@ -1,22 +1,23 @@
-/** biome-ignore-all lint/style/noNonNullAssertion: build script accessing manifest */
-/* eslint-disable @typescript-eslint/no-non-null-assertion, no-console */
+/* eslint-disable no-console */
 import { generateManifest } from 'material-icon-theme'
 import { mkdirSync, readFileSync, writeFileSync } from 'node:fs'
 import { resolve } from 'node:path'
 const manifest = generateManifest()
 const iconsDir = resolve(import.meta.resolve('material-icon-theme').replace('file://', ''), '../../../icons')
-const usedIcons = new Set<string>([
-  manifest.file!,
-  manifest.folder!,
-  manifest.folderExpanded!,
-  manifest.rootFolder!,
-  manifest.rootFolderExpanded!,
-  ...Object.values(manifest.folderNames!),
-  ...Object.values(manifest.folderNamesExpanded!),
-  ...Object.values(manifest.fileExtensions!),
-  ...Object.values(manifest.fileNames!),
-  ...Object.values(manifest.languageIds!)
-])
+const usedIcons = new Set<string>(
+  [
+    manifest.file ?? '',
+    manifest.folder ?? '',
+    manifest.folderExpanded ?? '',
+    manifest.rootFolder ?? '',
+    manifest.rootFolderExpanded ?? '',
+    ...Object.values(manifest.folderNames ?? {}),
+    ...Object.values(manifest.folderNamesExpanded ?? {}),
+    ...Object.values(manifest.fileExtensions ?? {}),
+    ...Object.values(manifest.fileNames ?? {}),
+    ...Object.values(manifest.languageIds ?? {})
+  ].filter(Boolean)
+)
 const svgMap: Record<string, string> = {}
 for (const name of usedIcons)
   try {
